c1c814acbc3389527b42f019c02773d4262d2ff5
Ryan D LaBarre <ryan@urbansitter.com>
2016-11-28 22:36:44 -0800

    Performance optimization: only need to loop through existing clean array once per element. Old "some"-based method could have been slightly faster in the odd case of most input records being duplicates, but this method is much better for real world data.

--- dedup.js
+++ dedup.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var file = __dirname + '/leads.json';
-var cleanLeads = { leads: [] };
+var clean = { leads: [] };
 
 fs.readFile(file, 'utf8', function (err, filedata) {
   if (err) {
@@ -15,41 +15,28 @@ fs.readFile(file, 'utf8', function (err, filedata) {
     return;
   }
 
-  // Compare the _id and email of each lead to all other leads; only put most recent unique leads into the clean output
+  // Loop through all leads, adding them to the clean array after checking whether there is a duplicate in the growing clean output.
+  // Replace that previously recorded entry with this newer one, unless the existing record had a newer entryDate.
   filedata.leads.map(function (lead) {
-    var leadDate = new Date(lead.entryDate);
-
-    var betterDup = filedata.leads.some(function (otherLead) {
-      if (lead._id === otherLead._id || lead.email === otherLead.email) {
-        var otherLeadDate = new Date(otherLead.entryDate);
-        if (otherLeadDate.getTime() > leadDate.getTime()) {
-            console.log('DUPLICATE DETECTED. Removing element with older entryDate:');
-            console.dir(lead);
+    for (var i = 0; i < clean.leads.length; i++) {
+      if (lead._id === clean.leads[i]._id || lead.email === clean.leads[i].email) {
+        var firstDate = new Date(clean.leads[i].entryDate);
+        var dupDate = new Date(lead.entryDate);
+        if (dupDate.getTime() >= firstDate.getTime()) {
+            console.log('DUPLICATE DETECTED. Removing older element:');
+            console.dir(clean.leads[i]);
             console.log('In favor of newer partial duplicate:');
-            console.dir(otherLead);
-            return true;
-        }
-        if (otherLeadDate.getTime() == leadDate.getTime()) {
-            if (filedata.leads.indexOf(lead) == filedata.leads.indexOf(otherLead)) {
-                return false; // skip inner loop comparing exact same item (not dup)
-            }
-            console.log('DUPLICATE ID/EMAIL AND ENTRY DATE DETECTED. Removing element provided sooner in array:');
             console.dir(lead);
-            console.log('In favor of partial duplicate:');
-            console.dir(otherLead);
-            filedata.leads.splice(filedata.leads.indexOf(lead), 1);
-            return true;
+            clean.leads.splice(i, 1);
+        }
+        else {
+            continue; // do not push this older duplicate into the clean output
         }
       }
-      // no better dup found yet; can potentially include this element in clean output
-      return false;
-    });
- 
-    if (betterDup === false) {
-      // entire map searched with no better dup found; include this lead data in clean output
-      cleanLeads.leads.push(lead);
     }
+    // no dup found
+    clean.leads.push(lead);
   });
 
-  fs.writeFileSync(__dirname + '/leads-clean.json', JSON.stringify(cleanLeads, null, ' '));
+  fs.writeFileSync(__dirname + '/leads-clean.json', JSON.stringify(clean, null, ' '));
 });
